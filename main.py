b71f512b61b6c27491a4e791cd29d68178b2fbe1
Shanith Perera <mail.shanith@gmail.com>
2017-03-19 23:12:01 +0530

    Creates a test QRCode

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from tkinter import *
 from PIL import Image, ImageTk
 import numpy as np
+import pyqrcode
 
 
+# Create a Tkinter GUI container
 root = Tk()
+root.wm_title("GhostTag")
 
-# lblName = Label(root, text="Hey there")
+# lblName = Label(root, text="Fuck shit up")
 # lblName.pack()
 
 cvs = Canvas(root, width=512, height=512)
@@ -18,13 +21,11 @@ data[256, 128] = [255, 0, 0]
 data[128, 256] = [255, 0, 0]
 data[128, 128] = [255, 0, 0]
 
-for i in range(1, 8):
-    for j in range(1, 8):
-        data[i, j] = [255, 0, 0]
-
-
 img = Image.fromarray(data, 'RGB')
 img.save('test.png')
+
+qr = pyqrcode.create("FUCK BITCHES!")
+qr.png("qr1.png", scale=8)
 
 img2 = Image.open("test.png")
 tk_img = ImageTk.PhotoImage(img2)
